90a1dd4cac20ee2dc5df33a955e0d5965d2c3162
AlexandrPetrikov <mojref@gmail.com>
2016-01-17 23:04:22 +0200

    add tab

--- js/index.js
+++ js/index.js
@@ -7,14 +7,15 @@ $(function(){
 $(document).ready(function(){
 
     /* code for block popup */
-   /*setTimeout(function(){
+   setTimeout(function(){
         $('#popup, #popup-overlay').fadeIn(100);
     },2000);
     $('#popup-overlay').click(function(){
         $('#popup, #popup-overlay').fadeOut(100);
-    });*/
-    /*************************/
+    });
+    /*-----------------------------------------*/
 
+    /* code for slider */
 	//configurations
 	var width = 1350;
 	var animationSpeed = 400;
@@ -44,4 +45,13 @@ $(document).ready(function(){
 	}
 	$slider.on('mouseenter', stopInterval).on('mouseleave', startSlider);
 	startSlider();
+    /*---------------------------------------*/
+
+    $('#tab-top-text').find('li').click(function(){
+        var currentNum = $(this).index();
+        $(this).addClass("show-tab").siblings().removeClass("show-tab");
+        $('.tab-content').eq(currentNum).addClass("show-tab-content")
+                         .siblings().removeClass("show-tab-content");
+    });
+
 });
